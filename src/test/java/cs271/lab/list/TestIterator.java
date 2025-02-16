07fc10f0d2802bd65aa3a4fd45754ab81825f6dd
MOHAMMED FOUZAN ALI <fouzanali409@gmail.com>
2025-02-16 02:37:50 -0600

    solved the errors

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -84,16 +84,16 @@ public class TestIterator {
     list.add(55);
     list.add(77);
     list.add(66);
-    final var i = list.iterator();
+     var i = list.iterator();
     while (i.hasNext()) {
-      if (i.next() == 77) {
+      if (i.next().equals(77)) {
         i.remove(); // TODO Question: What happens if you use list.remove(Integer.valueOf(77))?
       }
     }
     // TODO using assertEquals and List.of, express which values are left in the list
     // See TestList.java for examples of how to use List.of; also see the Java List
     // interface for more information
-    fail("Not yet implemented"); // remove this line when done
+    assertEquals(List.of(33, 44, 55, 66), list); // remove this line when done
   }
 
   @Test
